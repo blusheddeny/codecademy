1ea61296e514868de9e8e4feab68cb3973f36a73
Ingrid Eddeny Valdez <101751922+blusheddeny@users.noreply.github.com>
2022-07-28 22:30:34 -0500

    Create convertTemp.js
    Project that solidify basic concepts of Javascript in codeacademy

---
+++ convertTemp.js
@@ -0,0 +1,10 @@
+//set value to kelvin
+const kelvin = 0
+//assign value to celsius
+const celsius = kelvin - 273
+//calculate fahrenheit 
+//round result
+const fahrenheit = Math.floor(celsius * (9/5) + 32) 
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
+const newton = Math.floor(celsius * (33/100))
+console.log(`The temperature is ${newton} degrees Newton.`)
